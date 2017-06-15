ccdc9ef5684aa108f924ea60c66d7c33b2fd88b0
Jim Fisher <jameshfisher@gmail.com>
2017-06-15 12:50:26 +0100

    rename some things

--- FisherTree.hs
+++ FisherTree.hs
@@ -10,85 +10,85 @@ splitPrefix    xs     ys              = ([], xs, ys)
 
 type Height = Int  -- not required at runtime but useful for assertions
 
--- Represents a non-empty map!
 -- To represent an empty map, use Maybe (or similar).
-data FisherNode v
+data FisherNotEmpty v
   = FisherZero v  -- maps empty string to v
-  | FisherSucc Height [Char] (Either v (Map Char (FisherNode v)))
+  | FisherSucc Height [Char] (Either v (Map Char (FisherNotEmpty v)))
   deriving (Eq)
 
 -- Association lists are the simplest data structure to express a map.
 -- Haskell libraries use association lists as an exchange format.
-toList :: FisherNode v -> [(String, v)]
-toList (FisherZero v) = [("", v)]
-toList (FisherSucc _ prefix (Left v)) = [(prefix, v)]
-toList (FisherSucc _ prefix (Right m)) =
-  concatMap (\(c, f) -> map (\(s, v) -> (prefix ++ c:s, v)) (toList f)) $ M.toList m
+fisherNotEmptyToList :: FisherNotEmpty v -> [(String, v)]
+fisherNotEmptyToList (FisherZero v) = [("", v)]
+fisherNotEmptyToList (FisherSucc _ prefix (Left v)) = [(prefix, v)]
+fisherNotEmptyToList (FisherSucc _ prefix (Right m)) =
+  concatMap (\(c, f) -> map (\(s, v) -> (prefix ++ c:s, v)) (fisherNotEmptyToList f)) $ M.toList m
 
-isValidNonEmptyFisherNodeAtHeight :: Height -> FisherNode v -> Bool
-isValidNonEmptyFisherNodeAtHeight 0 (FisherZero _) = True
-isValidNonEmptyFisherNodeAtHeight h (FisherSucc h' prefix (Left _)) =
+fisherNotEmptyIsValidAtHeight :: Height -> FisherNotEmpty v -> Bool
+fisherNotEmptyIsValidAtHeight 0 (FisherZero _) = True
+fisherNotEmptyIsValidAtHeight h (FisherSucc h' prefix (Left _)) =
   h == h' && h == length prefix
-isValidNonEmptyFisherNodeAtHeight h (FisherSucc h' prefix (Right m)) =
+fisherNotEmptyIsValidAtHeight h (FisherSucc h' prefix (Right m)) =
   h == h' &&
   0 < length prefix && length prefix < h &&
   1 < num_branches && num_branches <= 256 &&
-  all (isValidNonEmptyFisherNodeAtHeight after_prefix_and_branches_height) (M.elems m)
+  all (fisherNotEmptyIsValidAtHeight after_prefix_and_branches_height) (M.elems m)
   where
     after_prefix_height = h - length prefix
     after_prefix_and_branches_height = after_prefix_height - 1
     num_branches = M.size m
-isValidNonEmptyFisherNodeAtHeight h f = False
+fisherNotEmptyIsValidAtHeight h f = False
 
-isValidFisherNodeAtHeight :: Height -> Maybe (FisherNode v) -> Bool
-isValidFisherNodeAtHeight h Nothing = True
-isValidFisherNodeAtHeight h (Just f) = isValidNonEmptyFisherNodeAtHeight h f
+fisherIsValidAtHeight :: Height -> Maybe (FisherNotEmpty v) -> Bool
+fisherIsValidAtHeight h Nothing = True
+fisherIsValidAtHeight h (Just f) = fisherNotEmptyIsValidAtHeight h f
 
-fisherFindNonEmpty :: String -> FisherNode v -> Maybe v
-fisherFindNonEmpty "" (FisherZero v) = Just v
-fisherFindNonEmpty s  (FisherSucc h s'     (Left v)) = if s == s' then Just v else Nothing
-fisherFindNonEmpty s  (FisherSucc h prefix (Right m)) =
+fisherNotEmptyFind :: String -> FisherNotEmpty v -> Maybe v
+fisherNotEmptyFind "" (FisherZero v) = Just v
+fisherNotEmptyFind s  (FisherSucc h s'     (Left v)) = if s == s' then Just v else Nothing
+fisherNotEmptyFind s  (FisherSucc h prefix (Right m)) =
   case stripPrefix prefix s of
     Nothing -> Nothing
     Just (c:rest) ->  case M.lookup c m of
                         Nothing -> Nothing
-                        Just f  -> fisherFindNonEmpty rest f
+                        Just f  -> fisherNotEmptyFind rest f
 
-fisherFind :: String -> Maybe (FisherNode v) ->  Maybe v
+fisherFind :: String -> Maybe (FisherNotEmpty v) ->  Maybe v
 fisherFind _ Nothing  = Nothing
-fisherFind s (Just f) = fisherFindNonEmpty s f
+fisherFind s (Just f) = fisherNotEmptyFind s f
 
-fisherSingleton :: String -> v -> FisherNode v
+fisherSingleton :: String -> v -> FisherNotEmpty v
 fisherSingleton "" v = FisherZero v
 fisherSingleton s v = FisherSucc (length s) s (Left v)
 
-fisherInsertNonEmpty :: String -> v -> FisherNode v -> FisherNode v
-fisherInsertNonEmpty "" v (FisherZero _) = FisherZero v
-fisherInsertNonEmpty s  v (FisherSucc h prefix next) =
+fisherNotEmptyInsert :: String -> v -> FisherNotEmpty v -> FisherNotEmpty v
+fisherNotEmptyInsert "" v (FisherZero _) = FisherZero v
+fisherNotEmptyInsert s  v (FisherSucc h prefix next) =
   case splitPrefix prefix s of
     (_      , [],        sRem  ) -> case next of
                                       Left _  -> FisherSucc h s (Left v)
                                       Right m -> FisherSucc h prefix (Right $ M.alter fn (head sRem) m) where
                                                   fn Nothing   = Just $ fisherSingleton (tail sRem) v
-                                                  fn (Just ft) = Just $ fisherInsertNonEmpty (tail sRem) v ft
+                                                  fn (Just ft) = Just $ fisherNotEmptyInsert (tail sRem) v ft
     (prefix', pc:prefCs, sc:sCs) -> FisherSucc h prefix' (Right $ M.fromList [
                                       (pc, FisherSucc (h - length prefix' - 1) prefCs next),
                                       (sc, FisherSucc (h - length prefix' - 1) sCs    (Left v))
                                     ])
 
+
 assertions :: Bool
 assertions = all id [
   splitPrefix "foo" "food" == ("foo", "", "d"),
   splitPrefix "foo" "bar" == ("", "foo", "bar"),
-  isValidNonEmptyFisherNodeAtHeight 0 (FisherZero "bar"),
-  isValidNonEmptyFisherNodeAtHeight 4 $ FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")],
-  isValidFisherNodeAtHeight 4 Nothing,
-  isValidFisherNodeAtHeight 4 $ Just $ FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")],
-  toList (FisherZero "bar") == [("", "bar")],
-  toList (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")]) == [("food", "food"), ("fool", "fool"), ("foot", "foot")],
-  fisherFindNonEmpty "food" (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")]) == Just "food",
-  fisherFindNonEmpty "foop" (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")]) == Nothing,
+  fisherNotEmptyIsValidAtHeight 0 (FisherZero "bar"),
+  fisherNotEmptyIsValidAtHeight 4 $ FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")],
+  fisherIsValidAtHeight 4 Nothing,
+  fisherIsValidAtHeight 4 $ Just $ FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")],
+  fisherNotEmptyToList (FisherZero "bar") == [("", "bar")],
+  fisherNotEmptyToList (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")]) == [("food", "food"), ("fool", "fool"), ("foot", "foot")],
+  fisherNotEmptyFind "food" (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")]) == Just "food",
+  fisherNotEmptyFind "foop" (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")]) == Nothing,
   fisherFind "food" Nothing == (Nothing :: Maybe String),
   fisherFind "food" (Just $ FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")]) == Just "food",
-  fisherInsertNonEmpty "fool" "fool" (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('t', FisherZero "foot")]) == (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")])
+  fisherNotEmptyInsert "fool" "fool" (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('t', FisherZero "foot")]) == (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherZero "food"), ('l', FisherZero "fool"), ('t', FisherZero "foot")])
   ]
