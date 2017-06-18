4589a0ee07171a306516ab148bb5b63e12bc5701
Jim Fisher <jameshfisher@gmail.com>
2017-06-18 10:39:14 +0100

    rename FisherSucc

--- FisherTree.hs
+++ FisherTree.hs
@@ -13,7 +13,7 @@ type Height = Int  -- not required at runtime but useful for assertions
 
 -- To represent an empty map, use Maybe (or similar).
 data FisherNotEmpty v
-  = FisherSucc Height [Char] (Either v (Map Char (FisherNotEmpty v)))
+  = FisherNotEmpty Height [Char] (Either v (Map Char (FisherNotEmpty v)))
   deriving (Eq, Show)
 
 type Fisher v = Maybe (FisherNotEmpty v)
@@ -21,8 +21,8 @@ type Fisher v = Maybe (FisherNotEmpty v)
 -- Association lists are the simplest data structure to express a map.
 -- Haskell libraries use association lists as an exchange format.
 fisherNotEmptyToList :: FisherNotEmpty v -> [(String, v)]
-fisherNotEmptyToList (FisherSucc _ prefix (Left v)) = [(prefix, v)]
-fisherNotEmptyToList (FisherSucc _ prefix (Right m)) =
+fisherNotEmptyToList (FisherNotEmpty _ prefix (Left v)) = [(prefix, v)]
+fisherNotEmptyToList (FisherNotEmpty _ prefix (Right m)) =
   concatMap (\(c, f) -> map (\(s, v) -> (prefix ++ c:s, v)) (fisherNotEmptyToList f)) $ M.toList m
 
 fisherToList :: Fisher v -> [(String, v)]
@@ -30,9 +30,9 @@ fisherToList Nothing = []
 fisherToList (Just f) = fisherNotEmptyToList f
 
 fisherNotEmptyIsValidAtHeight :: Height -> FisherNotEmpty v -> Bool
-fisherNotEmptyIsValidAtHeight h (FisherSucc h' prefix (Left _)) =
+fisherNotEmptyIsValidAtHeight h (FisherNotEmpty h' prefix (Left _)) =
   h == h' && h == length prefix
-fisherNotEmptyIsValidAtHeight h (FisherSucc h' prefix (Right m)) =
+fisherNotEmptyIsValidAtHeight h (FisherNotEmpty h' prefix (Right m)) =
   h == h' &&
   0 < length prefix && length prefix < h &&
   1 < num_branches && num_branches <= 256 &&
@@ -47,7 +47,7 @@ fisherIsValidAtHeight _ Nothing = True
 fisherIsValidAtHeight h (Just f) = fisherNotEmptyIsValidAtHeight h f
 
 fisherNotEmptyFind :: String -> FisherNotEmpty v -> Maybe v
-fisherNotEmptyFind s (FisherSucc _ prefix next) =
+fisherNotEmptyFind s (FisherNotEmpty _ prefix next) =
   case stripPrefix prefix s of
     Nothing -> Nothing
     Just rest -> case next of
@@ -61,19 +61,19 @@ fisherFind _ Nothing  = Nothing
 fisherFind s (Just f) = fisherNotEmptyFind s f
 
 fisherSingleton :: String -> v -> FisherNotEmpty v
-fisherSingleton s v = FisherSucc (length s) s (Left v)
+fisherSingleton s v = FisherNotEmpty (length s) s (Left v)
 
 fisherNotEmptyInsert :: String -> v -> FisherNotEmpty v -> FisherNotEmpty v
-fisherNotEmptyInsert s  v (FisherSucc h prefix next) =
+fisherNotEmptyInsert s  v (FisherNotEmpty h prefix next) =
   case splitPrefix prefix s of
     (_      , [],        sRem  ) -> case next of
-                                      Left _  -> FisherSucc h s (Left v)
-                                      Right m -> FisherSucc h prefix (Right $ M.alter fn (head sRem) m) where
+                                      Left _  -> FisherNotEmpty h s (Left v)
+                                      Right m -> FisherNotEmpty h prefix (Right $ M.alter fn (head sRem) m) where
                                                   fn Nothing   = Just $ fisherSingleton (tail sRem) v
                                                   fn (Just ft) = Just $ fisherNotEmptyInsert (tail sRem) v ft
-    (prefix', pc:prefCs, sc:sCs) -> FisherSucc h prefix' (Right $ M.fromList [
-                                      (pc, FisherSucc (h - length prefix' - 1) prefCs next),
-                                      (sc, FisherSucc (h - length prefix' - 1) sCs    (Left v))
+    (prefix', pc:prefCs, sc:sCs) -> FisherNotEmpty h prefix' (Right $ M.fromList [
+                                      (pc, FisherNotEmpty (h - length prefix' - 1) prefCs next),
+                                      (sc, FisherNotEmpty (h - length prefix' - 1) sCs    (Left v))
                                     ])
     _ -> error "inserted string must be different length to height of tree"
 
@@ -82,12 +82,12 @@ fisherInsert s v Nothing  = Just $ fisherSingleton s v
 fisherInsert s v (Just f) = Just $ fisherNotEmptyInsert s v f
 
 fisherNotEmptyDelete :: String -> FisherNotEmpty v -> Fisher v
-fisherNotEmptyDelete s t@(FisherSucc _ s' (Left _)) =
+fisherNotEmptyDelete s t@(FisherNotEmpty _ s' (Left _)) =
   if s == s' then Nothing else Just t
-fisherNotEmptyDelete s t@(FisherSucc h prefix (Right m)) = case splitPrefix prefix s of
+fisherNotEmptyDelete s t@(FisherNotEmpty h prefix (Right m)) = case splitPrefix prefix s of
   (_, [], (c:cs)) -> case M.toList newM of
-                      [(c', FisherSucc _ prefix' next)] -> Just $ FisherSucc h (prefix ++ [c'] ++ prefix') next
-                      _                                  -> Just $ FisherSucc h prefix (Right newM)
+                      [(c', FisherNotEmpty _ prefix' next)] -> Just $ FisherNotEmpty h (prefix ++ [c'] ++ prefix') next
+                      _                                  -> Just $ FisherNotEmpty h prefix (Right newM)
                      where
                       newM = M.alter fn c m where
                         fn Nothing = Nothing
@@ -102,19 +102,19 @@ assertions :: Bool
 assertions = all id [
   splitPrefix "foo" "food" == ("foo", "", "d"),
   splitPrefix "foo" "bar" == ("", "foo", "bar"),
-  fisherNotEmptyIsValidAtHeight 0 (FisherSucc 0 "" (Left "bar")),
-  fisherNotEmptyIsValidAtHeight 4 $ FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherSucc 0 "" (Left "food")), ('l', FisherSucc 0 "" (Left "fool")), ('t', FisherSucc 0 "" (Left "foot"))],
+  fisherNotEmptyIsValidAtHeight 0 (FisherNotEmpty 0 "" (Left "bar")),
+  fisherNotEmptyIsValidAtHeight 4 $ FisherNotEmpty 4 "foo" $ Right $ M.fromList [('d', FisherNotEmpty 0 "" (Left "food")), ('l', FisherNotEmpty 0 "" (Left "fool")), ('t', FisherNotEmpty 0 "" (Left "foot"))],
   fisherIsValidAtHeight 4 Nothing,
-  fisherIsValidAtHeight 4 $ Just $ FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherSucc 0 "" (Left "food")), ('l', FisherSucc 0 "" (Left "fool")), ('t', FisherSucc 0 "" (Left "foot"))],
-  fisherNotEmptyToList (FisherSucc 0 "" (Left "bar")) == [("", "bar")],
-  fisherNotEmptyToList (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherSucc 0 "" (Left "food")), ('l', FisherSucc 0 "" (Left "fool")), ('t', FisherSucc 0 "" (Left "foot"))]) == [("food", "food"), ("fool", "fool"), ("foot", "foot")],
-  fisherNotEmptyFind "food" (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherSucc 0 "" (Left "food")), ('l', FisherSucc 0 "" (Left "fool")), ('t', FisherSucc 0 "" (Left "foot"))]) == Just "food",
-  fisherNotEmptyFind "foop" (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherSucc 0 "" (Left "food")), ('l', FisherSucc 0 "" (Left "fool")), ('t', FisherSucc 0 "" (Left "foot"))]) == Nothing,
+  fisherIsValidAtHeight 4 $ Just $ FisherNotEmpty 4 "foo" $ Right $ M.fromList [('d', FisherNotEmpty 0 "" (Left "food")), ('l', FisherNotEmpty 0 "" (Left "fool")), ('t', FisherNotEmpty 0 "" (Left "foot"))],
+  fisherNotEmptyToList (FisherNotEmpty 0 "" (Left "bar")) == [("", "bar")],
+  fisherNotEmptyToList (FisherNotEmpty 4 "foo" $ Right $ M.fromList [('d', FisherNotEmpty 0 "" (Left "food")), ('l', FisherNotEmpty 0 "" (Left "fool")), ('t', FisherNotEmpty 0 "" (Left "foot"))]) == [("food", "food"), ("fool", "fool"), ("foot", "foot")],
+  fisherNotEmptyFind "food" (FisherNotEmpty 4 "foo" $ Right $ M.fromList [('d', FisherNotEmpty 0 "" (Left "food")), ('l', FisherNotEmpty 0 "" (Left "fool")), ('t', FisherNotEmpty 0 "" (Left "foot"))]) == Just "food",
+  fisherNotEmptyFind "foop" (FisherNotEmpty 4 "foo" $ Right $ M.fromList [('d', FisherNotEmpty 0 "" (Left "food")), ('l', FisherNotEmpty 0 "" (Left "fool")), ('t', FisherNotEmpty 0 "" (Left "foot"))]) == Nothing,
   fisherFind "food" Nothing == (Nothing :: Maybe String),
-  fisherFind "food" (Just $ FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherSucc 0 "" (Left "food")), ('l', FisherSucc 0 "" (Left "fool")), ('t', FisherSucc 0 "" (Left "foot"))]) == Just "food",
-  fisherNotEmptyInsert "fool" "fool" (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherSucc 0 "" (Left "food")), ('t', FisherSucc 0 "" (Left "foot"))]) == (FisherSucc 4 "foo" $ Right $ M.fromList [('d', FisherSucc 0 "" (Left "food")), ('l', FisherSucc 0 "" (Left "fool")), ('t', FisherSucc 0 "" (Left "foot"))]),
-  fisherInsert "food" (5::Integer) Nothing == (Just $ FisherSucc 4 "food" (Left 5)),
-  fisherNotEmptyDelete "bar" (FisherSucc 3 "" (Right (M.fromList [('b',FisherSucc 2 "ar" (Left (11 :: Integer))),('f',FisherSucc 2 "oo" (Left 5))]))) == Just (FisherSucc 3 "foo" (Left 5))
+  fisherFind "food" (Just $ FisherNotEmpty 4 "foo" $ Right $ M.fromList [('d', FisherNotEmpty 0 "" (Left "food")), ('l', FisherNotEmpty 0 "" (Left "fool")), ('t', FisherNotEmpty 0 "" (Left "foot"))]) == Just "food",
+  fisherNotEmptyInsert "fool" "fool" (FisherNotEmpty 4 "foo" $ Right $ M.fromList [('d', FisherNotEmpty 0 "" (Left "food")), ('t', FisherNotEmpty 0 "" (Left "foot"))]) == (FisherNotEmpty 4 "foo" $ Right $ M.fromList [('d', FisherNotEmpty 0 "" (Left "food")), ('l', FisherNotEmpty 0 "" (Left "fool")), ('t', FisherNotEmpty 0 "" (Left "foot"))]),
+  fisherInsert "food" (5::Integer) Nothing == (Just $ FisherNotEmpty 4 "food" (Left 5)),
+  fisherNotEmptyDelete "bar" (FisherNotEmpty 3 "" (Right (M.fromList [('b',FisherNotEmpty 2 "ar" (Left (11 :: Integer))),('f',FisherNotEmpty 2 "oo" (Left 5))]))) == Just (FisherNotEmpty 3 "foo" (Left 5))
   ]
 
 data Op
